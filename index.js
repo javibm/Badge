f9f4b6450a252468e6ade07870f73e4084f6a9f1
javibm <javibm6@gmail.com>
2017-02-13 19:54:02 +0100

    Added api status

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 app.use(bodyParser.json());
 
 PORT = 	process.env.PORT || "3005";
+var badge = require('gh-badges');
 
-app.get('/badge', function(req,res){
-
-	var badge = require('gh-badges');
+app.get('/runner/passing', function(req,res){
 	// Optional step, to have accurate text width computation.
 	badge.loadFont('/path/to/Verdana.ttf', function(err) {
-	  badge({ text: ["build", "passed"], colorscheme: "green", template: "flat" },
+	  badge({ text: ["Runner", "passing"], colorscheme: "green", template: "flat" },
 	    function(svg, err) {
-	     res.setHeader('content-type', 'image/svg+xml');
-	     res.send(svg);
+		     fs.writeFile("runner.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
+			    console.log("The file was saved!");
+			}); 
+	    });
+	});
+});
+
+app.get('/runner/failed', function(req,res){
+	// Optional step, to have accurate text width computation.
+	badge.loadFont('/path/to/Verdana.ttf', function(err) {
+	  badge({ text: ["Runner", "failed"], colorscheme: "red", template: "flat" },
+	    function(svg, err) {
+		     fs.writeFile("runner.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
+			    console.log("The file was saved!");
+			}); 
 	    });
 	});
 });
