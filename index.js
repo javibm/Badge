421e25fcb99cc8a6090875b8c62a52c2c64c2261
javibm <javibm6@gmail.com>
2017-02-13 20:00:37 +0100

    Added Dodge Pro

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ var badge = require('gh-badges');
 app.get('/runner/passing', function(req,res){
 	// Optional step, to have accurate text width computation.
 	badge.loadFont('/path/to/Verdana.ttf', function(err) {
-	  badge({ text: ["Runner", "passing"], colorscheme: "green", template: "flat" },
+	  badge({ text: ["Runner", "passing"], colorscheme: "green", template: "flat-square" },
 	    function(svg, err) {
 		     fs.writeFile("runner.html", svg, function(err) {
 			    if(err) {
 			        return console.log(err);
 			    }
 			    res.sendStatus(200);
-			    console.log("The file was saved!");
 			}); 
 	    });
 	});
@@ -27,14 +26,73 @@ app.get('/runner/passing', function(req,res){
 app.get('/runner/failed', function(req,res){
 	// Optional step, to have accurate text width computation.
 	badge.loadFont('/path/to/Verdana.ttf', function(err) {
-	  badge({ text: ["Runner", "failed"], colorscheme: "red", template: "flat" },
+	  badge({ text: ["Runner", "failed"], colorscheme: "red", template: "flat-square" },
 	    function(svg, err) {
 		     fs.writeFile("runner.html", svg, function(err) {
 			    if(err) {
 			        return console.log(err);
 			    }
 			    res.sendStatus(200);
-			    console.log("The file was saved!");
+			}); 
+	    });
+	});
+});
+
+app.get('/runner/running', function(req,res){
+	// Optional step, to have accurate text width computation.
+	badge.loadFont('/path/to/Verdana.ttf', function(err) {
+	  badge({ text: ["Runner", "running"], colorscheme: "blue", template: "flat-square" },
+	    function(svg, err) {
+		     fs.writeFile("runner.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
+			}); 
+	    });
+	});
+});
+
+app.get('/dodgepro/passing', function(req,res){
+	// Optional step, to have accurate text width computation.
+	badge.loadFont('/path/to/Verdana.ttf', function(err) {
+	  badge({ text: ["DodgePro", "passing"], colorscheme: "green", template: "flat-square" },
+	    function(svg, err) {
+		     fs.writeFile("dodgepro.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
+			}); 
+	    });
+	});
+});
+
+app.get('/dodgepro/failed', function(req,res){
+	// Optional step, to have accurate text width computation.
+	badge.loadFont('/path/to/Verdana.ttf', function(err) {
+	  badge({ text: ["DodgePro", "failed"], colorscheme: "red", template: "flat-square" },
+	    function(svg, err) {
+		     fs.writeFile("dodgepro.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
+			}); 
+	    });
+	});
+});
+
+app.get('/dodgepro/running', function(req,res){
+	// Optional step, to have accurate text width computation.
+	badge.loadFont('/path/to/Verdana.ttf', function(err) {
+	  badge({ text: ["DodgePro", "running"], colorscheme: "blue", template: "flat-square" },
+	    function(svg, err) {
+		     fs.writeFile("dodgepro.html", svg, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    res.sendStatus(200);
 			}); 
 	    });
 	});
@@ -43,6 +101,11 @@ app.get('/runner/failed', function(req,res){
 app.get('/runner', function(req,res){
     res.setHeader('content-type', 'image/svg+xml');
 	res.sendFile(__dirname + '/runner.html')
+});
+
+app.get('/dodgepro', function(req,res){
+    res.setHeader('content-type', 'image/svg+xml');
+	res.sendFile(__dirname + '/dodgepro.html')
 });
 
 
